d906b4afe603581a2b2bd67078fbac340de6ba3c
Byron Blocker <byronblocker@gmail.com>
2017-04-24 15:17:59 -0700

    create program that will add numbers

---
+++ AddIt.java
@@ -0,0 +1,24 @@
+import java.io.Console;
+
+public class AddIt {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+
+    System.out.println("Pick a number");
+    String strNum1 = myConsole.readLine();
+    System.out.println("Interesting... interesting...");
+    System.out.println("Okay now pick another one. Then I'll add them together!");
+    String strNum2 = myConsole.readLine();
+
+
+    Integer num1 = Integer.parseInt(strNum1);
+    Integer num2 = Integer.parseInt(strNum2);
+    Integer add = num1 + num2;
+    String total = Integer.toString(add);
+
+    System.out.println("Okay, you chose " + strNum1 + " and " + strNum2);
+    System.out.println("I guess added together that would be... hmm...");
+    System.out.println(total);
+
+  }
+}
