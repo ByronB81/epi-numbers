186683bd34f4718ac19d6b15979cb6102a89e680
Byron Blocker <byronblocker@gmail.com>
2017-04-24 17:36:26 -0700

    add flair to make it more fun

--- Encrypt.java
+++ Encrypt.java
@@ -7,12 +7,17 @@ public class Encrypt {
     System.out.println("Enter a phrase");
     String dirty = myConsole.readLine();
     String rinse1 = dirty.replaceAll("e", "3");
-    String rinse2 = rinse1.replaceAll("l", "1");
+    String rinse2 = rinse1.replaceAll("i", "1");
     String rinse3 = rinse2.replaceAll("s","5");
     String rinse4 = rinse3.replaceAll("o","0");
+    String rinse5 = rinse4.replaceAll("b","6");
+    String rinse6 = rinse5.replaceAll("g","9");
+    String clean = rinse5.replaceAll("l","7");
 
-
-    System.out.println(rinse4);
+    System.out.println();
+    System.out.println("...encrypting...");
+    System.out.println();
+    System.out.println(clean);
 
   }
 }
